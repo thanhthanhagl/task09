59b813458a9f112b9bf30ef51c757494b7577149
thanhthanhagl <diepthanhthanh.agl@gmail.com>
2022-11-30 17:47:20 +0700

    20%

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("main.js");
+$(document).ready(function () {
+    $('.c-mainnvisual__imgs').slick({
+        fade: true,
+        autoplay: true,
+        autoplayspeed: 6000,
+        speed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+
+});
