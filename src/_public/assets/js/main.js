31f746b394d09432ee30e77dbecdc4a23ff22003
thanhthanhagl <diepthanhthanh.agl@gmail.com>
2022-12-02 17:51:35 +0700

    50%

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //----------slick slider------------
     $('.c-mainnvisual__imgs').slick({
         fade: true,
         autoplay: true,
@@ -9,5 +10,27 @@ $(document).ready(function () {
         pauseOnFocus: false,
         pauseOnHover: false,
     });
+    //-----------tab--------------------
+    $('[name=tabs]').each(function (i, d) {
+        var p = $(this).prop('checked');
+        //   console.log(p);
+        if (p) {
+            $('.c-recruit__tab').eq(i)
+                .addClass('active');
+            $('.c-recruit__label').eq(i)
+                .addClass('active');
+        }
+    });
 
+    $('[name=tabs]').on('change', function () {
+        var p = $(this).prop('checked');
+
+        // $(type).index(this) == nth-of-type
+        var i = $('[name=tabs]').index(this);
+
+        $('.c-recruit__tab').removeClass('active');
+        $('.c-recruit__tab').eq(i).addClass('active');
+        $('.c-recruit__label').removeClass('active');
+        $('.c-recruit__label').eq(i).addClass('active');
+    });
 });
