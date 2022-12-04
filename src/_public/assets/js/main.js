47aa7191bf0d8d4a5dc84703629400290ecfde4f
thanhthanh54 <thanhuitk13@gmail.com>
2022-12-05 00:09:17 +0700

    done popup

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -33,4 +33,50 @@ $(document).ready(function () {
         $('.c-recruit__label').removeClass('active');
         $('.c-recruit__label').eq(i).addClass('active');
     });
+    //popup lightbox
+    var popup = $(".c-lightbox");
+    var img = $(".c-works__photo");
+    var popupImage = $(".c-lightbox img");
+    var overlay = $('.c-lightbox__overlay');
+    var closeBtn = $(".c-lightbox__close");
+  
+    img.on("click", function () {
+      var img_src = $(this).find("img").attr("src");
+      popup.find("img").attr("src", img_src);
+      popup.addClass("is-open");
+      $('body').addClass('is-fixed');
+    });
+  
+    $(closeBtn).on("click", function () {
+        popup.removeClass("is-open");
+      $('body').removeClass('is-fixed');
+    });
+    $(overlay).on("click", function () {
+        popup.removeClass("is-open");
+      $('body').removeClass('is-fixed');
+    });
+  
+    popupImage.on("click", function (e) {
+      e.stopPropagation();
+    });
+    //popup interview
+    $("[data-open]").on("click", function () {
+        var popup_name = $(this).attr("data-open");
+        $('[data-popup="' + popup_name + '"]').fadeIn(300);
+        $('.c-popup').addClass('is-open');
+        $('body').addClass('is-fixed');
+      });
+    
+      $("[data-popup-close]").on("click", function () {
+        var popup_name = $(this).attr("data-popup-close");
+        $('[data-popup="' + popup_name + '"]').fadeOut(300);
+        $('.c-popup').removeClass('is-open');
+        $('body').removeClass('is-fixed');
+      });
+    
+      $(".c-popup__overlay").on("click", function () {
+        $('.c-popup').removeClass('is-open');
+        $('body').removeClass('is-fixed');
+        $('.c-popup__box').fadeOut(300);
+      });
 });
